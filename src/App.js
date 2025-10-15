967237cbd4e49009fb0cd0ab6df04aa7b4f9f8f2
zahra Ershad1 <z83ershad@gmail.com>
2025-10-15 12:01:15 +0330

    Auto Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
+
+
 const App =() => {
   return (
     <div className="App">
      <h3>
       Contact Manager
      </h3>
+     <br />
+     <button className='btn btn-danger'>
+      دکمه
+     </button>
+     <br />
+     <span className='fa fa-user'></span>
     </div>
   );
 }
