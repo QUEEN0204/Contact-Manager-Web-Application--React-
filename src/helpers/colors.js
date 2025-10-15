ce36a588b4b9728a9546fa532d8b444fb47bd9b5
zahra Ershad1 <z83ershad@gmail.com>
2025-10-15 16:23:39 +0330

    create colors.js

---
+++ src/helpers/colors.js
@@ -0,0 +1,12 @@
+export const Background = "#282A36";
+export const CurrentLine = "#6272A4";
+export const Selection = "#44475A";
+export const Foreground = "#F8F8F2";
+export const Comment = "#6272A4";
+export const Red = "#FF5555";
+export const Orange = "#FFB86C";
+export const Yellow = "#F1FA8C";
+export const Green = "#50FA7B";
+export const Cyan = "#8BE9FD";
+export const Purple = "#BD93F9";
+export const Pink = "#FF79C6";
