46837759bfb19bdb5921188334a612825e8b0dcc
zahra Ershad1 <z83ershad@gmail.com>
2025-10-28 16:28:43 +0330

    create Contact.jsx and move it's code to Contact.jsx

---
+++ src/components/Contacts/Contact.jsx
@@ -0,0 +1,65 @@
+import { Pink, Purple,Red} from "../../helpers/colors";
+
+
+const Contact = () =>{
+    return(
+        <div className="col-md-6">
+        <div className="card my-2" style={{background:'#282a47db'}}>
+            <div className="card-body">
+                <div className="row align-items-center d-flex justify-content-around">
+                    <div className="col-md-4 col-sm-4">
+                        <img src="https://placehold.co/200" alt="" 
+                        style={{border:`1px solid ${Purple}`}} className="img-fluid rounded" />
+                    </div>
+                    <div className="col-md-7 col-sm-7">
+                        <ul className="list-group">
+                            <li className="list-group-item list-group-item-dark">
+                                نام و نام خانوادگی: {' '}
+
+                                <span className="fw-bold mx-1">
+                                    زهرا ارشاد
+                                </span>
+
+                            </li>
+                            <li className="list-group-item list-group-item-dark">
+                                 شماره تلفن  : {' '}
+
+                                <span className="fw-bold mx-1">
+                                    09123456789
+                                </span>
+
+                            </li>
+                            <li className="list-group-item list-group-item-dark">
+                                آدرس ایمیل: {' '}
+
+                                <span className="fw-bold mx-1">
+                                    zahra@gmail.com
+                                </span>
+
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center">
+                        <button className="btn my-1" style={{backgroundColor:Purple}}>
+
+                            <i className="fa fa-eye"/>
+                        </button>
+                        <button className="btn my-1" style={{backgroundColor:Pink}}>
+
+                            <i className="fa fa-edit"/>
+                        </button>
+                        <button className="btn my-1" style={{backgroundColor:Red}}>
+
+                            <i className="fa fa-trash"/>
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    )
+}
+export default Contact;
