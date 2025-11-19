3ac4a48d5bbd22d06fe5c485e47ae6d154870cad
zahra Ershad1 <z83ershad@gmail.com>
2025-11-19 18:38:16 +0330

    added 1th addContact structure

--- src/components/Contacts/AddContact.jsx
+++ src/components/Contacts/AddContact.jsx
@@ -1,8 +1,72 @@
-const AddContact = () =>{
+import { Link } from "react-router-dom";
+
+import {Spinner} from '../../assets/spinner.gif';
+import {Register} from '../../assets/register.gif';
+import {} from '../../helpers/colors';
+
+import '../../App.css';
+
+const AddContact= ({loading})=>{
     return(
-        <h1>
-            Add contact
-        </h1>
+        <>
+        {
+            loading?(<Spinner />):(
+                <>
+                <section className="p-3">
+                    <img src={Register} className="regGif" alt="Regiter form" />
+                </section>
+
+                <div className="container">
+                    <div className="rwo">
+                        <div className="col">
+                            <p className="h4 fw-bold text-center">
+                                ایجاد مخاطب جدید
+                            </p>
+
+                        </div>
+                    </div>
+                    <hr className="bg-purple" />
+                        <div className="row mt-5">
+                            <div className="col-md-4">
+                                <form>
+                                    <div>
+                                        <input type="text" name="fullName" placeholder="" id="" required={true} />
+                                    </div>
+                                    <div>
+                                        <input type="text" name="photo" placeholder="" id="" required={true} />
+                                    </div>
+                                    <div>
+                                        <input type="number" name="mobile" placeholder="" id="" required={true} />
+                                    </div>
+                                    <div>
+                                        <input type="email" name="email" placeholder="" id="" required={true} />
+                                    </div>
+                                    <div>
+                                        <input type="text" name="job" placeholder="" id="" required={true} />
+                                    </div>
+                                    <div>
+                                        <select name="group" id="">
+                                            <option value="">
+                                                انتخاب گروه
+                                            </option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <input type="submit" value="createContact" />
+                                        <Link to={'/contacts'}>
+                                        بازگشت
+                                        </Link>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        
+                </div>
+
+                </>
+            )
+        }
+
+        </>
     )
 }
-export default AddContact;
