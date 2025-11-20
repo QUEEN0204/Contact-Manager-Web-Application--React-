29cd4da74f9efdf34321026019b186a4f785cdc4
zahra Ershad1 <z83ershad@gmail.com>
2025-11-20 11:51:28 +0330

    added style for viewContact.jsx

--- src/components/Contacts/ViewContact.jsx
+++ src/components/Contacts/ViewContact.jsx
@@ -1,37 +1,34 @@
 import { useEffect, useState } from "react";
-import { Link,useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
-import { getContact , getGroup } from "../../services/contactService";
+import { getContact, getGroup } from "../../services/contactService";
 import Spinner from '../Spinner';
-import { Comment } from "../../helpers/colors";
 
+import "../../App.css";
 
-const ViewContact = () =>{
-    const {contactId}=useParams();
-
-    const [state,setState] = useState({
-        loading:false ,
-        contact:{},
-        group:{}
+const ViewContact = () => {
+    const { contactId } = useParams();
+    const [state, setState] = useState({
+        loading: false,
+        contact: {},
+        group: {}
     });
 
-    useEffect(()=>{
-        const fetchData = async()=>{
-            try{
-                setState({ ...state, loading:true });
-                const {data:contactData} = await getContact(contactId);
-                const {data:groupData} = await getGroup(contactData.groups);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setState({ ...state, loading: true });
+                const { data: contactData } = await getContact(contactId);
+                const { data: groupData } = await getGroup(contactData.groups);
 
                 setState({
                     ...state,
-                    loading:false,
-                    contact:contactData,
-                    group:groupData
+                    loading: false,
+                    contact: contactData,
+                    group: groupData
                 });
-
-            }
-            catch(e){
-                setState({ ...state, loading:false });
+            } catch (e) {
+                setState({ ...state, loading: false });
                 console.log(e.message);
             }
         }
@@ -39,77 +36,68 @@ const ViewContact = () =>{
         fetchData();
     }, [contactId]);
 
-    const{loading, contact , group} =state;
+    const { loading, contact, group } = state;
+    
 
-    return(
-       <>
-       <section>
-            <div className="container">
-                <div className="row">
-                    <p>
-                        اطلاعات مخاطب
-                    </p>
-                </div>
-            </div>
-       </section>
-
-       <hr />
-       {
-        loading? (<Spinner />):(
-            <>
-            <div className="viewcontact">
+    return (
+        <>
+            <section style={{ backgroundColor: '#282A36', padding: '1rem 0' }}>
                 <div className="container">
                     <div className="row">
-                        <div className="col">
-                            <img src={contact.photo} alt="" />
-                        </div>
-                        <div className="col">
-                            <div>
-                                <h5>
-                                    نام و نام خانوادگی
-                                </h5>
-                                <h4>
-                                {contact.fullName}
-                                </h4>
-                            </div>
-                             <div>
-                                <h5>
-                                    شماره تلفن
-                                </h5>
-                                <h4>
-                                {contact.mobile}
-                                </h4>
-                            </div>
-                             <div>
-                                <h5>
-                                    شغل
-                                </h5>
-                                <h4>
-                                {contact.job}
-                                </h4>
-                            </div>
-                             <div>
-                                <h5>
-                                   نسبت
-                                </h5>
-                                <h4>
-                                {contact.groups}
-                                </h4>
-                            </div>
-                        </div>
-                        <Link to={'/contacts'}>
-                        بازگشت به صفحه اصلی
-                        </Link>
+                        <p style={{ 
+                            color: '#F8F8F2', 
+                            fontSize: '1.5rem', 
+                            margin: 0,
+                            fontWeight: '600'
+                        }}>
+                            اطلاعات مخاطب
+                        </p>
                     </div>
                 </div>
-            </div>
-    
-            </>
-        )
+            </section>
 
-       }
-
-       </>
+            <hr style={{ borderColor: '#6272A4', margin: 0 }} />
+            
+            {
+                loading ? (<Spinner />) : (
+                    <>
+                        <div className="viewcontact">
+                            <div className="container">
+                                <div className="row">
+                                    <div className="col">
+                                        <img src={contact.photo} alt={contact.fullName} />
+                                    </div>
+                                    <div className="col">
+                                        <div className="contact-info">
+                                            <div>
+                                                <h5>نام و نام خانوادگی</h5>
+                                                <h4>{contact.fullName}</h4>
+                                            </div>
+                                            <div>
+                                                <h5>شماره تلفن</h5>
+                                                <h4>{contact.mobile}</h4>
+                                            </div>
+                                            <div>
+                                                <h5>شغل</h5>
+                                                <h4>{contact.job}</h4>
+                                            </div>
+                                            <div>
+                                                <h5>نسبت</h5>
+                                                <h4>{group.name}</h4>
+                                            </div>
+                                        </div>
+                                        <Link to={'/contacts'} className="back-button">
+                                            بازگشت به صفحه اصلی
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+        </>
     )
 }
+
 export default ViewContact;
